e3e905e1ea8eccade234486b2df11415ab51d0ad
FranciscoJunior12 <105241871+FranciscoJunior12@users.noreply.github.com>
2023-09-15 23:06:13 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+import './App.css'
+import HelloWorld from './components/HelloWorld'
+import Contador from './components/Contador'
+import ToDoList from './components/ToDoList'
+import CicloDeVida from './components/CicloDeVida'
+
+function App() {
+
+
+  return (
+    <div>
+      <CicloDeVida />
+    </div>
+  )
+}
+
+export default App;
---
+++ src/components/Componet2.jsx
@@ -0,0 +1,12 @@
+
+const Component2 = () => {
+    return (
+        <>
+            <h2>Nome: <span>Francisco Manuel Domingos Junior</span></h2>
+            <h2>Idade: <span>21</span></h2>
+
+        </>
+    )
+}
+
+export default Component2;
